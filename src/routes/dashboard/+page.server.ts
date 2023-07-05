dbec77863725aeb7a1310a3f3364dc58d001447b
jecktor <jecktor@proton.me>
2023-07-05 06:24:24 -0600

    Wip: create board

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,5 +1,9 @@
 import { redirect, type Actions, fail } from '@sveltejs/kit';
+import { get } from 'svelte/store';
 import { auth } from '$lib/server';
+import { db } from '$lib/server';
+import { randomId } from '$utils';
+import { locale, translate } from '$locales';
 
 export const load = async ({ locals }) => {
 	const { user } = await locals.auth.validateUser();
@@ -11,10 +15,26 @@ export const load = async ({ locals }) => {
 };
 
 export const actions: Actions = {
-	default: async ({ locals }) => {
+	signout: async ({ locals }) => {
 		const session = await locals.auth.validate();
 		if (!session) return fail(401);
 		await auth.invalidateSession(session.sessionId);
 		locals.auth.setSession(null);
+	},
+	newboard: async ({ locals }) => {
+		const { user } = await locals.auth.validateUser();
+		if (!user) throw redirect(302, '/login');
+
+		try {
+			await db.execute('CALL create_board(?,?,?,?)', [
+				randomId(),
+				translate(get(locale), 'newboard'),
+				new Date(),
+				user.userId
+			]);
+		} catch (error) {
+			console.error(error);
+			return fail(500);
+		}
 	}
 };
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -9,9 +9,14 @@
 	<h1>{data.user.name}</h1>
 	<img src={data.user.image} alt="avatar" width="50" height="50" />
 </div>
-<a href="/live">{$t('continues')}</a>
 <a href="/dashboard/settings">{$t('settings')}</a>
 
-<form use:enhance method="post">
+<form use:enhance method="post" action="?/signout">
 	<input type="submit" value={$t('signout')} />
 </form>
+
+<div>
+	<form action="?/newboard" method="post">
+		<button type="submit">{$t('createboard')}</button>
+	</form>
+</div>
