b6bd333c76c99426d2d0d14423828f4058055ff5
yorsh130204 <jorge130204@gmail.com>
2023-07-07 16:18:26 -0600

    Update +page.svelte
    Adding css and assets to teh first page of kolabr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,12 @@
 
 <style></style>
 
-<h1>{$t('herotitle')}</h1>
+<section class="landing">
+	<div class="navbar" >
+		<a class="btn-success" href="/login">{$t('login')}</a>
+		<a href="/signup">{$t('signup')}</a>
+		<h1>{$t('herotitle')}</h1>
 <a href="/login">{$t('login')}</a>
 <a href="/signup">{$t('signup')}</a>
+	</div>
+</section>
