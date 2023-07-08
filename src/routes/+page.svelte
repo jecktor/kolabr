43a517682100c3350b8e4819c5c7e1bd96b9b2c7
yorsh130204 <jorge130204@gmail.com>
2023-07-08 17:23:56 -0600

    Page.svelte
    adding css,img,etc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,17 +3,18 @@
 </script>
 
 <style>
-	@font-face {
-		font-family: 'Inter';
-		src: url('/ruta/de/tu/fuente/Inter-Regular.woff2') format('woff2'),
-			url('/ruta/de/tu/fuente/Inter-Regular.woff') format('woff');
-		font-weight: normal;
-		font-style: normal;
-		font-display: swap;
-	}
+	@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
 
 	.landing {
-		width: 80%;
+		background-color: #ffffff;
+		height: 10px;
+		width: auto;
+		padding: 0px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		position: relative;
 	}
 
 	.logo {
@@ -21,9 +22,9 @@
 		height: 28px;
 		width: 128px;
 		padding: 0;
-		left: 80px;
-		top: 25px;
 		position: absolute;
+		top: 25px;
+		left: 80px;
 	}
 
 	.buttons-container {
@@ -54,10 +55,9 @@
 	}
 
 	.button2 {
-		width: 91px;
-		height: 38px;
 		background-color: #f4ca64;
 		color: #fcfef4;
+		margin-left: 20px;
 	}
 
 	.button3 {
@@ -69,16 +69,11 @@
 		font-size: 24px;
 		background-color: #f4ca64;
 		color: #fcfef4;
-		padding-left: 24px;
-		padding-right: 24px;
-		padding-top: 16px;
-		padding-bottom: 16px;
+		padding: 16px 24px;
 		display: flex;
-		flex-direction: row;
 		justify-content: center;
 		align-items: center;
 		gap: 8px;
-		position: relative;
 		border-style: hidden;
 		outline: none;
 	}
@@ -87,15 +82,25 @@
 		position: absolute;
 		width: 123px;
 		height: 123px;
-		left: 750px;
+		left: 983px;
 		top: 142px;
 		background: #fb37ff;
 		filter: blur(100px);
 	}
 
+	.ellipse133 {
+		position: absolute;
+		width: 123px;
+		height: 123px;
+		left: 1311px;
+		top: 671px;
+		background: #18B2DE;
+		filter: blur(100px);
+	}
+
 	.a {
 		position: absolute;
-		width: 637px;
+		width: 807px;
 		height: 328px;
 		left: 80px;
 		top: 237px;
@@ -115,25 +120,51 @@
 		top: 589px;
 		color: #4d4d4d;
 		text-align: left;
-		vertical-align: text-top;
 		font-size: 24px;
 		font-family: 'Inter', sans-serif;
 		letter-spacing: 0.5px;
 		line-height: 36px;
-		border-style: hidden;
-		outline: none;
-		width: 637px;
 	}
 
-	.c {
+	.img1 {
 		position: absolute;
 		width: 468px;
 		height: 427px;
-		left: 750px;
+		left: 921px;
 		top: 245px;
 		max-width: 100%;
 		height: auto;
 	}
+
+	.img2 {
+		position: absolute;
+		width: 98px;
+		height: 62px;
+		left: 780px;
+		top: 1288px;
+	}
+
+	.img3 {
+		position: absolute;
+		width: 632px;
+		height: 708px;
+		left: 995px;
+		top: 925px;
+	}
+
+	.img4 {
+		position: absolute;
+		width: 498px;
+		height: 396px;
+		left: 177px;
+		top: 1811px;
+	}
+
+	.img-responsive:hover {
+        transform: scale(1.1);
+		transition: transform 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+        cursor: pointer;
+    }
 
 	.paragraph {
 		position: absolute;
@@ -143,41 +174,151 @@
 		top: 989px;
 	}
 
+	.paragraph2 {
+		position: absolute;
+		width: 820px;
+		height: 190px;
+		left: 80px;
+		top: 1279px;
+	}
+
+	.paragraph3 {
+		position: absolute;
+		width: 820px;
+		height: 226px;
+		left: 828px;
+		top: 1783px;
+	}
+
+	.paragraph4 {
+		position: absolute;
+		width: 820px;
+		height: 190px;
+		left: 828px;
+		top: 2109px;
+	}
+
+	.paragraph5 {
+		position: absolute;
+		width: 853px;
+		height: 190px;
+		left: 358px;
+		top: 80px;
+	}
+
 	.d {
-		text-align:left;
-		vertical-align:text-top;
-		line-height:auto;
-		border-style:hidden;
-		outline:none;
-		width:560px;
+		text-align: left;
 		font-family: 'Inter', sans-serif;
-		font-style: normal;
 		font-weight: 600;
 		font-size: 48px;
 		line-height: 58px;
 		color: #212121;
 	}
 
-	.e {}
+	.e {
+		width: 820px;
+		height: 108px;
+		left: 80px;
+		top: 1076px;
+		color: #4d4d4d;
+		text-align: left;
+		font-size: 24px;
+		font-family: 'Inter', sans-serif;
+		letter-spacing: 0.5px;
+		line-height: 36px;
+		margin-top: 20px;
+	}
+
+	.f {
+		text-align: right;
+	}
+
+	.g {
+		text-align: center;
+	}
+
+	.rectangle316 {
+		position: absolute;
+		width: 1292px;
+		height: 53px;
+		left: 218px;
+		top: 1681px;
+		background: linear-gradient(93.51deg, #9B51E0 2.84%, #3081ED 99.18%);
+		filter: blur(100px);
+		border-radius: 10px;
+	}
+
+	.footer {
+		box-sizing: border-box;
+		position: absolute;
+		width: 1568px;
+		height: 454px;
+		left: 80px;
+		top: 2499px;
+		border-top: 1px solid #D3D3D3;
+	}
+
+	.h {
+		position: absolute;
+		width: 140px;
+		height: 24px;
+		left: 714px;
+		top: 350px;
+		font-family: 'Inter';
+		font-style: normal;
+		font-weight: 400;
+		font-size: 20px;
+		line-height: 24px;
+		text-align: center;
+		color: #7A7A7A;
+	}
 
 	@media (max-width: 768px) {
+		/* Estilos adicionales para dispositivos móviles... */
 	}
 </style>
+
 <section class="landing">
-	<div><img class="logo" src="/Logo.png" alt=""></div>
-	<div class="buttons-container">
-		<a class="button1" href="/login">{$t('login')}</a>
-		<a class="button2" href="/signup">{$t('signup')}</a>
-</div>
-<div>
-	<div class='ellipse132'></div>
-	<h1 class="a">{$t('herotitle')}</h1>
-	<h3 class="b">{$t('herodesc')}</h3>
-	<div class="c"><img src="Group 12.png" alt=""></div>
-	<a class="button3" href="/login">{$t('getstarted')}</a>
-	<div class='paragraph'>
-		<div class="d">{$t('feat1title')}</div>
-		<div class="e">{$t('feat1desc')}</div>
+	<div>
+		<img class="logo img-fluid img-responsive" src="/Logo.png" alt="">
 	</div>
-</div>	
+	<div class="buttons-container">
+		<a class="button1 btn btn-secondary" href="/login">{$t('login')}</a>
+		<a class="button2 btn btn-secondary" href="/signup">{$t('signup')}</a>
+	</div>
+	<div class="container">
+		<p class="ellipse132"></p>
+		<p class="ellipse133"></p>
+		<h1 class="a">{$t('herotitle')}</h1>
+		<h3 class="b">{$t('herodesc')}</h3>
+		<img class="img1 img-fluid img-responsive" src="Group 12.png" alt="">
+	</div>
+	<a class="button3 btn btn-secondary" href="/login">{$t('getstarted')}</a>
+	<div class="paragraph">
+		<h1 class="d">{$t('feat1title')}</h1>
+		<h3 class="e">{$t('feat1desc')}</h3>
+	</div>
+	<div class="paragraph2">
+		<h1 class="d">{$t('feat2title')}</h1>
+		<h3 class="e">{$t('feat2desc')}</h3>
+	</div>
+	<p class="rectangle316"></p>
+	<img class="img2 img-fluid img-responsive" src="Group 6.png" alt="">
+	<img class="img3 img-fluid img-responsive" src="Group 11.png" alt="">
+	<div class="paragraph3">
+		<h1 class="d f">{$t('feat3title')}</h1>
+		<h3 class="e f">{$t('feat3desc')}</h3>
+	</div>
+	<div class="paragraph4">
+		<h1 class="d f">{$t('feat4title')}</h1>
+		<h3 class="e f">{$t('feat4desc')}</h3>
+	</div>
+	<img class="img4 img-fluid img-responsive" src="Group 10.png" alt="">
 </section>
+<footer class="footer">
+	<div class="paragraph5">
+		<h1 class="d g">{$t('footertitle')}</h1>
+		<h3 class="e g">{$t('footerdesc')}</h3>
+	</div>
+	<p class="h">Kolabr &copy; 2023</p>
+</footer>
