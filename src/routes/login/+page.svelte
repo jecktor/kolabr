cc8fc39da607cf54ca45ccfd8d90c59b953e3ac4
yorsh130204 <jorge130204@gmail.com>
2023-07-10 11:35:30 -0600

    Login
    Login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -22,13 +22,8 @@
 		border: 1px solid #D3D3D3;
 		border-radius: 12px;
 		position: relative;
-		top: 50%;
+		top: 45%;
 	}
-
-	.btn-primary {
-		width: 100%;
-	}
-
 	.a {
 		margin-top: 20px;
 		width: 100px;
@@ -39,13 +34,33 @@
 		font-size: 32px;
 		line-height: 20px;
 	}
-
 	.b {
 		font-family: 'Inter';
 		font-style: normal;
 		font-weight: 400;
 		font-size: 16px;
 		line-height: 20px;
+		margin-bottom: 15px;
+	}
+	.c {
+		width: 100%;
+		height: 40px;
+		font-size: 16px;
+	}
+	.button {
+		align-items: center;
+		padding: 10px 16px;
+		gap: 8px;
+		background: #f4ca64;
+		border-radius: 6px;
+		font-family: 'Inter', sans-serif;
+		font-weight: 600;
+		font-size: 16px;
+		line-height: 18px;
+		color: #fdf3d7;
+		text-decoration: none;
+		border: none;
+		width: 100%;
 	}
 </style>
 
@@ -57,18 +72,18 @@
 				<form method="post" use:enhance>
 					<div class="form-group mb-4">
 						<label class="b" for="email">{$t("email")}</label>
-						<input type="email" id="email" name="email" class="form-control" required>
+						<input type="email" id="email" name="email" class="form-control c" required>
 					</div>
 					<div class="form-group mb-4">
 						<label class="b" for="password">{$t("pass")}</label>
-						<input type="password" id="password" name="password" class="form-control" required>
+						<input type="password" id="password" name="password" class="form-control c" required>
 					</div>
-					<button type="submit" class="btn btn-primary btn-lg btn-block">{$t("continues")}</button>
+					<button type="submit" class="button btn btn-secondary btn-lg btn-block">{$t("continues")}</button>
 				</form>
 				{#if form?.message}
-				<p>{$t(form.message)}</p>
+				<p class="b">{$t(form.message)}</p>
 				{/if}
-				<a class="text-center" href="/signup">Create an account</a>
+				<a class="b text-center link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="/signup">{$t("create")}</a>
 			</div>
 		</div>
 	</div>
