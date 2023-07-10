04bfa3a8a1c2cef2df7040b4dd7e40c2c421c4ec
JaviierSM <134535376+JaviierSM@users.noreply.github.com>
2023-07-10 11:59:37 -0600

    signin
    Cambio al diseño

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -21,3 +21,46 @@
 	<p>{$t(form.message)}</p>
 {/if}
 <a href="/login">{$t('login')}</a>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+
+	.signup-wrapper{
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: column;
+		padding: 40px;
+		gap: 25px;
+		width: 100%;
+		max-width: 498px;
+		margin: auto;
+		background: #FFFFFF;
+		border: 1px solid #D3D3D3;
+		border-radius: 12px;
+		position: relative;
+		top: 50%;
+	}
+
+	.btn-primary{
+		width: 100%;
+	}
+
+	.a{
+		margin-top: 20px;
+		width: 100px;
+		height: 20px;
+		font-family: 'Inter';
+		font-style: normal;
+		font-weight: 600;
+		font-size: 32px;
+		line-height: 20px;
+	}
+
+	.b {
+		font-family: 'Inter';
+		font-style: normal;
+		font-weight: 400;
+		font-size: 16px;
+		line-height: 20px;
+	}
+</style>
