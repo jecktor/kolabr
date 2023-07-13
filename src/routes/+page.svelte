195d8a83131fb6b35c3cf98257d6e070bb294750
yorsh130204 <jorge130204@gmail.com>
2023-07-12 18:24:46 -0600

    Initial Page Finished
    The first page is finally finished.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,9 @@
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
-
 	.a {
-		width: 807px auto;
-		height: 328px auto;
+		width: 100%;
+		height: auto;
 		color: #212121;
 		font-family: 'Inter', sans-serif;
 		font-weight: 600;
@@ -16,10 +15,9 @@
 		letter-spacing: -2px;
 		text-align: left;
 	}
-
 	.b {
-		width: 637px auto;
-		height: 72px auto;
+		width: 100%;
+		height: auto;
 		color: #4d4d4d;
 		text-align: left;
 		font-size: 24px;
@@ -27,70 +25,18 @@
 		letter-spacing: 0.5px;
 		line-height: 36px;
 	}
-
-	.img1 {
+	.img {
 		width: 100%;
 		max-width: 468px;
 		height: auto;
 		max-height: 427px;
 	}
-
-	.img2 {
-		width: 98px;
-		height: 62px;
-		left: 780px;
-		top: 1288px;
+	.img1 {
+		width: 100%;
+		max-width: 632px;
+		height: auto;
+		max-height: 708px;
 	}
-
-	.img3 {
-		width: 632px;
-		height: 708px;
-		left: 995px;
-		top: 925px;
-	}
-
-	.img4 {
-		width: 498px;
-		height: 396px;
-		left: 177px;
-		top: 1811px;
-	}
-
-	.paragraph {
-		width: 820px;
-		height: 190px;
-		left: 80px;
-		top: 989px;
-	}
-
-	.paragraph2 {
-		width: 820px;
-		height: 190px;
-		left: 80px;
-		top: 1279px;
-	}
-
-	.paragraph3 {
-		width: 820px;
-		height: 226px;
-		left: 828px;
-		top: 1783px;
-	}
-
-	.paragraph4 {
-		width: 820px;
-		height: 190px;
-		left: 828px;
-		top: 2109px;
-	}
-
-	.paragraph5 {
-		width: 853px;
-		height: 190px;
-		left: 358px;
-		top: 80px;
-	}
-
 	.d {
 		text-align: left;
 		font-family: 'Inter', sans-serif;
@@ -99,53 +45,24 @@
 		line-height: 58px;
 		color: #212121;
 	}
-
-	.e {
-		width: 820px;
-		height: 108px;
-		left: 80px;
-		top: 1076px;
+	.g {
+		text-align: center;
+	}
+	.footer {
+		border-top: 1px solid #D3D3D3;
+	}
+	.h {
+		width: 100%;
+		height: auto;
 		color: #4d4d4d;
 		text-align: left;
 		font-size: 24px;
 		font-family: 'Inter', sans-serif;
 		letter-spacing: 0.5px;
 		line-height: 36px;
-		margin-top: 20px;
-	}
-
-	.f {
-		text-align: right;
-	}
-
-	.g {
-		text-align: center;
-	}
-
-	.footer {
-		box-sizing: border-box;
-		position: absolute;
-		width: 1568px;
-		height: 454px;
-		left: 80px;
-		top: 2499px;
-		border-top: 1px solid #D3D3D3;
-	}
-
-	.h {
-		width: 140px;
-		height: 24px;
-		left: 714px;
-		top: 350px;
-		font-family: 'Inter';
-		font-style: normal;
-		font-weight: 400;
-		font-size: 20px;
-		line-height: 24px;
 		text-align: center;
 		color: #7A7A7A;
 	}
-
 	header {
 		height: 20%;
 		display: flex;
@@ -205,6 +122,155 @@
 		border-style: hidden;
 		outline: none;
 	}
+	.space {
+		margin-bottom: 5%;
+	}
+	.space1 {
+		margin-bottom: 5%;
+	}
+	.space2 {
+		margin-top: 5%;
+	}
+	.space3 {
+		margin-left: 20px;
+	}
+	.img-responsive:hover {
+        transform: scale(1.1);
+		transition: transform 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+        cursor: pointer;
+    }
+	@media (max-width: 768px) {
+		header {
+			padding-inline: 15px;
+			padding-block: 12px;
+			margin-bottom: 10%;
+			width: 100%;
+		}
+		.a {
+			width: 90%;
+			height: auto;
+			color: #212121;
+			font-family: 'Inter', sans-serif;
+			font-weight: 600;
+			font-size: 38px;
+			line-height: 50px;
+			letter-spacing: -2px;
+			text-align: left;
+			margin-left: 10px;
+		}
+		.b {
+			width: 90%;
+			height: auto;
+			color: #4d4d4d;
+			text-align: left;
+			font-size: 14px;
+			font-family: 'Inter', sans-serif;
+			letter-spacing: 0.5px;
+			line-height: 18px;
+			margin-left: 10px;
+		}
+		.img {
+			width: 90%;
+			max-width: 327px;
+			height: auto;
+			max-height: 299px;
+		}
+		.img1 {
+			width: 90%;
+			max-width: 355px;
+			height: auto;
+			max-height: 398px;
+		}
+		.c {
+			width: auto;
+		}
+		.d {
+			text-align: left;
+			font-family: 'Inter', sans-serif;
+			font-weight: 600;
+			font-size: 18px;
+			line-height: auto;
+			color: #212121;
+			margin-left: 10px;
+			margin-bottom: 2px;
+			height: 30px;
+		}
+		.buttons {
+			width: auto;
+			height: 40px auto;
+			border-radius: 90px;
+		}
+		.button1,
+		.button2,
+		.button3 {
+			background: #fdf3d7;
+			font-family: 'Inter', sans-serif;
+			font-size: 14px;
+			color: #caa53d;
+			border-style: none;
+			justify-content: center;
+			align-items: center;
+			gap: 8px;
+			outline: none;
+		}
+		.button2 {
+			background-color: #f4ca64;
+			color: #fcfef4;
+		}
+		.button3 {
+			width: 129px;
+			height: 40px;
+			background-color: #f4ca64;
+			color: #fcfef4;
+			padding: 10px 20px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			gap: 8px;
+			border-style: hidden;
+			outline: none;
+			margin-left: 10px;
+		}
+		.space {
+			margin-bottom: 10%;
+		}
+		.space1 {
+			margin-bottom: 1px;
+		}
+		.space2 {
+			margin-top: 5%;
+		}
+		.space3 {
+			margin-left: 3vh;
+		}
+		.img-responsive:hover {
+			transform: scale(1.1);
+			transition: transform 0.3s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+			cursor: pointer;
+		}
+		.g {
+			text-align: center;
+		}
+		.footer {
+			border-top: 1px solid #D3D3D3;
+		}
+		.h {
+			width: 90%;
+			height: auto;
+			color: #4d4d4d;
+			font-size: 14px;
+			font-family: 'Inter', sans-serif;
+			letter-spacing: 0.5px;
+			line-height: 18px;
+			text-align: center;
+			color: #7A7A7A;
+			margin-left: auto;
+			margin-right: auto;
+		}
+		.f {
+			width: 65%;
+		}
+	}
 </style>
 
 <header>
@@ -216,100 +282,53 @@
 	<div class="header_node">
 		<div class="buttons">
 			<a class="button1 btn btn-secondary" href="/login">{$t('login')}</a>
-          	<a class="button2 btn btn-secondary" href="/signup">{$t('signup')}</a>
+          	<a class="button2 btn btn-secondary space3" href="/signup">{$t('signup')}</a>
 		</div>
 	</div>
 </header>
-<div class="row justify-content-center">
-	<div class="col-md-8">
+<div class="space row justify-content-center">
+	<div class="col-md-8 justify-content-center">
 		<h1 class="a">{$t('herotitle')}</h1><br>
-		<h3 class="b">{$t('herodesc')}</h3><br>
+		<h3 class="b f">{$t('herodesc')}</h3><br>
 	</div>
-	<div class="col-md-8">
+	<div class="space col-md-8 justify-content-center">
 		<a class="button3 btn btn-secondary" href="/login">{$t('getstarted')}</a>
 	</div>
-	<div class="row justify-content-center text-center">
+	<div class="space row justify-content-center text-center">
 		<div class="col-md-6">
-			<img class="img1 img-fluid img-responsive" src="Group 12.png" alt="">
+			<img class="img img-fluid img-responsive" src="Group 12.png" alt="">
 		</div>
 	</div>
+	<div class="space1 col-md-8">
+		<h1 class="d">{$t('feat1title')}</h1><br>
+		<h3 class="b c">{$t('feat1desc')}</h3><br>
+	</div>
+	<div class="space1 col-md-8">
+		<h1 class="d">{$t('feat2title')}</h1><br>
+		<h3 class="b c">{$t('feat2desc')}</h3><br>
+	</div>
+	<div class="space row justify-content-center text-center">
+		<div class="col-md-6">
+
+			<img class="img1 img-fluid img-responsive" src="Group 11.png" alt="">
+		</div>
+	</div>
+	<div class="space1 col-md-8">
+		<h1 class="d">{$t('feat3title')}</h1><br>
+		<h3 class="b c">{$t('feat3desc')}</h3><br>
+	</div>
+	<div class="space1 col-md-8">
+		<h1 class="d">{$t('feat4title')}</h1><br>
+		<h3 class="b c">{$t('feat4desc')}</h3><br>
+	</div>
+	<div class="space row justify-content-center text-center">
+		<div class="col-md-6">
+			<img class="img img-fluid img-responsive" src="Group 10.png" alt="">
+		</div>
+	</div>
+	<div class="footer col-md-10">
+		<h1 class="d g space2">{$t('footertitle')}</h1><br>
+		<h3 class="h">{$t('footerdesc')}</h3><br>
+		<p class="h space2">Kolabr &copy; 2023</p>
+	</div>
 </div>
-<!-- <article class="landing">
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-6">
-        <img class="logo img-fluid" src="Logo.png" alt="">
-      </div>
-      <div class="col-md-6 d-flex align-items-center justify-content-end">
-        <div class="buttons-container">
-          <a class="button1 btn btn-secondary" href="/login">{$t('login')}</a>
-          <a class="button2 btn btn-secondary" href="/signup">{$t('signup')}</a>
-        </div>
-      </div>
-    </div>
-    <div class="row justify-content-center">
-      <div class="col-md-8 ">
-        <h1 class="a">{$t('herotitle')}</h1>
-        <h3 class="b">{$t('herodesc')}</h3>
-      </div>
-    </div>
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <img class="img1 img-fluid img-responsive" src="Group 12.png" alt="">
-      </div>
-    </div>
-    <div class="row justify-content-center mt-4">
-      <div class="col-md-6 text-center">
-        <a class="button3 btn btn-secondary" href="/login">{$t('getstarted')}</a>
-      </div>
-    </div>
-    <div class="row justify-content-center mt-4">
-      <div class="col-md-8 text-center">
-        <div class="paragraph">
-          <h1 class="d">{$t('feat1title')}</h1>
-          <h3 class="e">{$t('feat1desc')}</h3>
-        </div>
-        <div class="paragraph2">
-          <h1 class="d">{$t('feat2title')}</h1>
-          <h3 class="e">{$t('feat2desc')}</h3>
-        </div>
-      </div>
-    </div>
-    <div class="row justify-content-center mt-4">
-      <div class="col-md-2 text-center">
-        <img class="img2 img-fluid img-responsive" src="Group 6.png" alt="">
-      </div>
-      <div class="col-md-4 text-center">
-        <img class="img3 img-fluid img-responsive" src="Group 11.png" alt="">
-      </div>
-      <div class="col-md-6 text-center">
-        <div class="paragraph3">
-          <h1 class="d f">{$t('feat3title')}</h1>
-          <h3 class="e f">{$t('feat3desc')}</h3>
-        </div>
-        <div class="paragraph4">
-          <h1 class="d f">{$t('feat4title')}</h1>
-          <h3 class="e f">{$t('feat4desc')}</h3>
-        </div>
-      </div>
-    </div>
-    <div class="row justify-content-center mt-4">
-      <div class="col-md-8 text-center">
-        <img class="img4 img-fluid img-responsive" src="Group 10.png" alt="">
-      </div>
-    </div>
-  </div>
-</article>
-<footer class="footer">
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8 text-center">
-        <div class="paragraph5">
-          <h1 class="d g">{$t('footertitle')}</h1>
-          <h3 class="e g">{$t('footerdesc')}</h3>
-        </div>
-        <p class="h">Kolabr &copy; 2023</p>
-      </div>
-    </div>
-  </div>
-</footer>  -->
