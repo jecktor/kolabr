0d2cfdc14ff5728678dd55d39e37f85fba42d102
yorsh130204 <jorge130204@gmail.com>
2023-07-17 21:07:15 -0600

    Dashboard
    Adding style, structure, and more

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -27,6 +27,9 @@
 	.space3 {
 		margin-bottom: 6%;
 	}
+	.space4 {
+		margin-bottom: 4.5%;
+	}
 	.a {
 		width: 90%;
 		height: auto;
@@ -70,6 +73,17 @@
 	.table-light {
 		height: 50px;
 	}
+	@media (max-width: 760px) {
+		.space2 {
+			margin-bottom: 7%;
+		}
+		.space3 {
+			margin-bottom: 12%;
+		}
+		.space4 {
+			margin-bottom: 8%;
+		}
+	}
 </style>
 
 <div class="container row justify-content-center space1">
@@ -86,88 +100,100 @@
 	
 	{#if data.ownerBoards.length > 0}
 		<section>
-			<strong><p class="a space2">{$t('ownerboards')}</p></strong>
-			<table class="table">
-				<tr>
-					<td><strong>{$t('name')}</strong></td>
-					<td><strong>{$t('lastedited')}</strong></td>
-					<td><strong>{$t('owner')}</strong></td>
-					<td><strong>{$t('actions')}</strong></td>
-				</tr>
-				<br>
-				{#each data.ownerBoards as board (board.id)}
-					<tr class="table-light space2">
-						<td><strong><a href={`/board/${board.id}`}>{board.name}</a></strong></td>
-						<td>{translateDate(board.last_edited)}</td>
-						<td>{data.user.name}</td>
-						<td>
-							<div class="button-group">
-								<button
-									aria-label={$t('copylink')}
-									title="{$t('copylink')}"
-									on:click={() =>
-										navigator.clipboard.writeText(`${window.location.origin}/board/${board.id}`)}
-								>
-									<div class="icon">
-										<FaLink />
+			<strong><p class="a space4">{$t('ownerboards')}</p></strong>
+			<div class="table-responsive">
+				<table class="table space2">
+					<thead>
+						<tr>
+							<th scope="col">{$t('name')}</th>
+							<th scope="col">{$t('lastedited')}</th>
+							<th scope="col">{$t('owner')}</th>
+							<th scope="col">{$t('actions')}</th>
+						</tr>
+					</thead>
+					<tbody>
+						{#each data.ownerBoards as board (board.id)}
+							<tr class="table-light space2">
+								<td><strong><a href={`/board/${board.id}`}>{board.name}</a></strong></td>
+								<td>{translateDate(board.last_edited)}</td>
+								<td>{data.user.name}</td>
+								<td>
+									<div class="button-group">
+										<button
+											aria-label={$t('copylink')}
+											title="{$t('copylink')}"
+											on:click={() =>
+												navigator.clipboard.writeText(`${window.location.origin}/board/${board.id}`)}
+										>
+											<div class="icon">
+												<FaLink />
+											</div>
+										</button>
+										<form use:enhance action="?/deleteboard" method="post">
+											<input type="hidden" name="board" value={board.id} />
+											<button type="submit" aria-label={$t('deletes')} title="{$t('deletes')}">
+												<div class="icon">
+													<FaTrash />
+												</div>
+											</button>
+										</form>
 									</div>
-								</button>
-								<form use:enhance action="?/deleteboard" method="post">
-									<input type="hidden" name="board" value={board.id} />
-									<button type="submit" aria-label={$t('deletes')} title="{$t('deletes')}">
-										<div class="icon">
-											<FaTrash />
-										</div>
-									</button>
-								</form>
-							</div>
-						</td>
-					</tr>
-					<br>
-				{/each}
-			</table>
+								</td>
+							</tr>
+							<br>
+						{/each}
+					</tbody>
+				</table>
+			</div>
 		</section>
 	{/if}
 	
 	{#if data.userBoards.length > 0}
 		<section>
-			<strong><p class="a space2">{$t('userboards')}</p></strong>
-			<table class="table">
-				<tr>
-					<td><strong>{$t('name')}</strong></td>
-					<td><strong>{$t('lastedited')}</strong></td>
-					<td><strong>{$t('owner')}</strong></td>
-					<td><strong>{$t('actions')}</strong></td>
-				</tr>
-				{#each data.userBoards as board (board.id)}
-					<tr class="table-light space2">
-						<td><a href={`/board/${board.id}`}>{board.name}</a></td>
-						<td>{translateDate(board.last_edited)}</td>
-						<td>{board.owner_name}</td>
-						<td>
-							<div class="button-group">
-								<button
-									aria-label={$t('copylink')}
-									on:click={() =>
-										navigator.clipboard.writeText(`${window.location.origin}/board/${board.id}`)}
-								>
-									<div class="icon">
-										<FaLink />
+			<strong><p class="a space4">{$t('userboards')}</p></strong>
+			<div class="table-responsive">
+				<table class="table space2">
+					<thead>
+						<tr>
+							<th scope="col">{$t('name')}</th>
+							<th scope="col">{$t('lastedited')}</th>
+							<th scope="col">{$t('owner')}</th>
+							<th scope="col">{$t('actions')}</th>
+						</tr>
+					</thead>
+					<tbody>
+						{#each data.userBoards as board (board.id)}
+							<tr class="table-light space2">
+								<td><a href={`/board/${board.id}`}>{board.name}</a></td>
+								<td>{translateDate(board.last_edited)}</td>
+								<td>{board.owner_name}</td>
+								<td>
+									<div class="button-group">
+										<button
+											aria-label={$t('copylink')}
+											title="{$t('copylink')}"
+											on:click={() =>
+												navigator.clipboard.writeText(`${window.location.origin}/board/${board.id}`)}
+										>
+											<div class="icon">
+												<FaLink />
+											</div>
+										</button>
+										<form action="?/removeboard" method="post">
+											<input type="hidden" name="board" value={board.id} />
+											<button type="submit" aria-label={$t('remove')} title="{$t('remove')}">
+												<div class="icon">
+													<FaTimes />
+												</div>
+											</button>
+										</form>
 									</div>
-								</button>
-								<form action="?/removeboard" method="post">
-									<input type="hidden" name="board" value={board.id} />
-									<button type="submit" aria-label={$t('remove')}>
-										<div class="icon">
-											<FaTimes />
-										</div>
-									</button>
-								</form>
-							</div>
-						</td>
-					</tr>
-				{/each}
-			</table>
+								</td>
+							</tr>
+						{/each}
+					</tbody>
+				</table>
+			</div>
 		</section>
 	{/if}
 	
