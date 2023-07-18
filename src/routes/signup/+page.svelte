e78280f4335ad6dc4333e0c1d92c35260d2836ad
jecktor <jecktor@proton.me>
2023-07-18 12:47:59 -0600

    Updated login styles

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -5,68 +5,6 @@
 	export let form: { message?: TranslationKeys };
 </script>
 
-
-<style>
-	@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
-
-	.container {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-	}
-	.signup-wrapper {
-		box-sizing: border-box;
-		display: flex;
-		flex-direction: column;
-		padding: 40px;
-		gap: 25px;
-		width: 100%;
-		max-width: 600px;
-		background: #FFFFFF;
-		border: 1px solid #D3D3D3;
-		border-radius: 12px;
-	}
-	.a {
-		margin-top: 20px;
-		width: 100%;
-		height: 20px;
-		font-family: 'Inter';
-		font-style: normal;
-		font-weight: 600;
-		font-size: 32px;
-		line-height: 20px;
-	}
-	.b {
-		font-family: 'Inter';
-		font-style: normal;
-		font-weight: 400;
-		font-size: 16px;
-		line-height: 20px;
-		margin-bottom: 5%;
-	}
-	.c {
-		width: 100%;
-		height: 40px;
-		font-size: 16px;
-	}
-	.button {
-		align-items: center;
-		padding: 10px 16px;
-		gap: 8px;
-		background: #f4ca64;
-		border-radius: 6px;
-		font-family: 'Inter', sans-serif;
-		font-weight: 600;
-		font-size: 16px;
-		line-height: 18px;
-		color: #fdf3d7;
-		text-decoration: none;
-		border: none;
-		width: 100%;
-	}
-</style>
-
 <div class="container signup">
 	<div class="row justify-content-center">
 		<div>
@@ -87,15 +25,96 @@
 					</div>
 					<div class="form-group mb-4">
 						<label class="b" for="passconfirm">{$t('passconfirm')}</label>
-						<input type="password" id="passconfirm" name="passconfirm" class="form-control c" required />
+						<input
+							type="password"
+							id="passconfirm"
+							name="passconfirm"
+							class="form-control c"
+							required
+						/>
 					</div>
-					<button type="submit" class="button btn btn-secondary btn-lg btn-block">{$t('continues')}</button>
+					<button type="submit" class="button btn btn-secondary btn-lg btn-block"
+						>{$t('continues')}</button
+					>
 				</form>
-				{#if form?.message}
-					<p class="b alert alert-danger">{$t(form.message)}</p>
-				{/if}
-				<a href="/login" class="b text-center link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">{$t('login')}</a>
+				<a
+					href="/login"
+					class="b text-center link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+					>{$t('login')}</a
+				>
 			</div>
 		</div>
 	</div>
+	{#if form?.message}
+		<p class="b alert alert-danger">{$t(form.message)}</p>
+	{/if}
 </div>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	.signup-wrapper {
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: column;
+		padding: 40px;
+		gap: 25px;
+		width: 500px;
+		background: var(--base-100);
+		border: 1px solid var(--base-300);
+		border-radius: 12px;
+	}
+
+	.a {
+		margin-top: 20px;
+		width: 100%;
+		height: 20px;
+		font-family: 'Inter';
+		font-style: normal;
+		font-weight: 600;
+		font-size: 32px;
+		line-height: 20px;
+	}
+
+	.b {
+		font-family: 'Inter';
+		font-style: normal;
+		font-weight: 400;
+		font-size: 16px;
+		line-height: 20px;
+		margin-bottom: 5%;
+	}
+
+	.c {
+		width: 100%;
+		height: 40px;
+		font-size: 16px;
+	}
+
+	.button {
+		align-items: center;
+		padding: 10px 16px;
+		gap: 8px;
+		background: #f4ca64;
+		border-radius: 6px;
+		font-family: 'Inter', sans-serif;
+		font-weight: 600;
+		font-size: 16px;
+		line-height: 18px;
+		color: #fdf3d7;
+		text-decoration: none;
+		border: none;
+		width: 100%;
+	}
+
+	@media (max-width: 768px) {
+		.signup-wrapper {
+			width: 100%;
+		}
+	}
+</style>
